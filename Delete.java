697f588b75ee37f358c11599c550b79b8fe947af
suresh3993 <156241930+suresh3993@users.noreply.github.com>
2024-04-03 01:19:20 +0530

    Add files via upload

---
+++ Delete.java
@@ -0,0 +1,28 @@
+package crud;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Delete extends Conn {
+    Delete() throws ClassNotFoundException,SQLException
+    {
+        super();
+    }
+    public void delete(int id) throws SQLException
+    {
+        String q="delete from sinfo where id=?";
+       
+        PreparedStatement ps=super.con.prepareStatement(q);
+        ps.setInt(1, id);
+       
+        int i=ps.executeUpdate();
+        if(i!=0)
+        {
+            System.out.println("delete Sucss");
+        }else
+        {
+
+            System.out.println("delete UnSucss");
+        }
+    }
+
+}
